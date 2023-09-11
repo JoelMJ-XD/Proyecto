7e9e11f6e1046f4a346389e0c7e2775cb9b88929
AlexFloRz26 <142933155+AlexFloRz26@users.noreply.github.com>
2023-09-11 14:30:15 -0600

    Create Footer.js
    pie

---
+++ Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2023 Tu Empresa. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
